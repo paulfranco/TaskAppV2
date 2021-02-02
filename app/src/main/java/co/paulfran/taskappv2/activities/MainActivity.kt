1db1a33947284b8da50f0f42e61cefce297b70ab
Paul Franco <mail@paulfran.co>
2021-02-02 01:04:05 -0800

    Creating New Projects

--- app/src/main/java/co/paulfran/taskappv2/activities/MainActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/MainActivity.kt
@@ -2,17 +2,62 @@ package co.paulfran.taskappv2.activities
 
 
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.text.InputType
+import android.view.View
+import android.widget.EditText
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import co.paulfran.taskappv2.adapters.ProjectAdapter
 import co.paulfran.taskappv2.R
+import co.paulfran.taskappv2.data.AppData
 import co.paulfran.taskappv2.databinding.ActivityMainBinding
+import co.paulfran.taskappv2.models.Project
+
 
 class MainActivity : BaseActivity() {
     lateinit var binding: ActivityMainBinding
+    private var projectsAdapter: ProjectAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.projectsRv.layoutManager = LinearLayoutManager(this)
+
+        AppData.initialize()
+
+        projectsAdapter = ProjectAdapter(AppData.projects)
+        binding.projectsRv.adapter = projectsAdapter
 
     }
+
+    fun createNewProject(v: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("New Project")
+        builder.setMessage("Please enter a name for the new project")
+
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_CLASS_TEXT
+        builder.setView(input)
+
+        builder.setPositiveButton("Save") { _, _ ->
+            val projectName: String = input.text.toString()
+            val newProject = Project(projectName, mutableListOf())
+
+            AppData.projects.add(newProject)
+            projectsAdapter!!.notifyItemInserted(AppData.projects.count())
+
+        }
+
+        builder.setNegativeButton("Cancel") { _, _ ->
+
+        }
+
+        val dialgue: AlertDialog = builder.create()
+        dialgue.show()
+    }
 }
+
+
+
