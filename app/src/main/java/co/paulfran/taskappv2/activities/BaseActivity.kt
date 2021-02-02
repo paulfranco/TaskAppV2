804c9946bf669e3dc2994d64bf8b76c79a29061a
Paul Franco <mail@paulfran.co>
2021-02-01 22:06:37 -0800

    group and item rows layouts created

--- app/src/main/java/co/paulfran/taskappv2/activities/BaseActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/BaseActivity.kt
@@ -1,11 +1,12 @@
-package co.paulfran.taskappv2
+package co.paulfran.taskappv2.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.paulfran.taskappv2.R
 
-class MainActivity : BaseActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
--- app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
@@ -1,8 +1,9 @@
-package co.paulfran.taskappv2
+package co.paulfran.taskappv2.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import co.paulfran.taskappv2.R
 import co.paulfran.taskappv2.databinding.ActivityItemsBinding
 
 class ItemsActivity : AppCompatActivity() {
