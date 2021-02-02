37aebdc1f27c0e064d128e5af0c1502008e7f45e
Paul Franco <mail@paulfran.co>
2021-02-02 04:28:17 -0800

    add a new item and hide keyboard

--- app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
@@ -1,24 +1,32 @@
 package co.paulfran.taskappv2.activities
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import co.paulfran.taskappv2.R
 import co.paulfran.taskappv2.adapters.ItemsAdapter
 import co.paulfran.taskappv2.data.AppData
 import co.paulfran.taskappv2.databinding.ActivityItemsBinding
+import co.paulfran.taskappv2.listeners.OnItemClickedListener
+import co.paulfran.taskappv2.models.Item
+import co.paulfran.taskappv2.models.Project
 
-class ItemsActivity : AppCompatActivity() {
+class ItemsActivity : AppCompatActivity(), OnItemClickedListener {
 
     lateinit var binding: ActivityItemsBinding
+    lateinit var thisProject: Project
+    var itemsAdapter: ItemsAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_items)
 
-        var selectedIndex = intent.getIntExtra("projectIndex", 0)
-        var thisProject = AppData.projects[selectedIndex]
+        val selectedIndex = intent.getIntExtra("projectIndex", 0)
+        thisProject = AppData.projects[selectedIndex]
 
         binding.toolbarTitle.text = thisProject.name
 
@@ -27,9 +35,26 @@ class ItemsActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayShowTitleEnabled(false)
 
         binding.itemsRv.layoutManager = LinearLayoutManager(this)
-        var itemsAdapter = ItemsAdapter(thisProject)
+        itemsAdapter = ItemsAdapter(thisProject, this)
 
         binding.itemsRv.adapter = itemsAdapter
+
+        binding.newItemEt.setOnKeyListener { view, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                if (event.action == KeyEvent.ACTION_DOWN) {
+                    val name: String = binding.newItemEt.text.toString()
+                    val item = Item(name, false)
+                    thisProject.items.add(item)
+                    itemsAdapter!!.notifyItemInserted(thisProject.items.count())
+                    binding.newItemEt.text.clear()
+
+                    // hide keyboard
+                    val inputManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                    inputManager.hideSoftInputFromWindow(view.windowToken, 0)
+                }
+            }
+            false
+        }
 
 
     }
@@ -37,5 +62,14 @@ class ItemsActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun itemClicked(index: Int) {
+        thisProject.items[index].completed = !thisProject.items[index].completed
+        itemsAdapter!!.notifyDataSetChanged()
+    }
+
+    override fun itemLongClicked(index: Int) {
+        //
     }
 }
