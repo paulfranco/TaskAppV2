145762ce64efe293ef6141d13298438468ee6889
Paul Franco <mail@paulfran.co>
2021-02-02 01:46:03 -0800

    onclick listeners and navigate from project list to item list

--- app/src/main/java/co/paulfran/taskappv2/activities/MainActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package co.paulfran.taskappv2.activities
 
 
 import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
@@ -13,10 +14,11 @@ import co.paulfran.taskappv2.adapters.ProjectAdapter
 import co.paulfran.taskappv2.R
 import co.paulfran.taskappv2.data.AppData
 import co.paulfran.taskappv2.databinding.ActivityMainBinding
+import co.paulfran.taskappv2.listeners.OnProjectClickedListener
 import co.paulfran.taskappv2.models.Project
 
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(), OnProjectClickedListener {
     lateinit var binding: ActivityMainBinding
     private var projectsAdapter: ProjectAdapter? = null
 
@@ -27,7 +29,7 @@ class MainActivity : BaseActivity() {
 
         AppData.initialize()
 
-        projectsAdapter = ProjectAdapter(AppData.projects)
+        projectsAdapter = ProjectAdapter(AppData.projects, this)
         binding.projectsRv.adapter = projectsAdapter
 
     }
@@ -56,6 +58,15 @@ class MainActivity : BaseActivity() {
 
         val dialgue: AlertDialog = builder.create()
         dialgue.show()
+    }
+
+    override fun projectClicked(index: Int) {
+        val intent = Intent(this, ItemsActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun projectLongClickedListener(index: Int) {
+        // Delete group
     }
 }
 
