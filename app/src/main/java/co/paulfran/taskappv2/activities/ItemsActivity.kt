95551760b14400f1fe561d4873fe41c14a87ca58
Paul Franco <mail@paulfran.co>
2021-02-02 03:16:47 -0800

    ItemsAdapter.kt and ItemsViewHolder.kt

--- app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
+++ app/src/main/java/co/paulfran/taskappv2/activities/ItemsActivity.kt
@@ -3,7 +3,9 @@ package co.paulfran.taskappv2.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import co.paulfran.taskappv2.R
+import co.paulfran.taskappv2.adapters.ItemsAdapter
 import co.paulfran.taskappv2.data.AppData
 import co.paulfran.taskappv2.databinding.ActivityItemsBinding
 
@@ -23,6 +25,11 @@ class ItemsActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        binding.itemsRv.layoutManager = LinearLayoutManager(this)
+        var itemsAdapter = ItemsAdapter(thisProject)
+
+        binding.itemsRv.adapter = itemsAdapter
 
 
     }
