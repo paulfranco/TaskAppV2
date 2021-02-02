ffa13197601b16ab3a80e8e66737dac34e120002
Paul Franco <mail@paulfran.co>
2021-02-02 10:31:49 -0800

    updated AppData

--- app/src/main/java/co/paulfran/taskappv2/data/AppData.kt
+++ app/src/main/java/co/paulfran/taskappv2/data/AppData.kt
@@ -1,24 +1,33 @@
 package co.paulfran.taskappv2.data
 
-import co.paulfran.taskappv2.models.Item
-import co.paulfran.taskappv2.models.Project
+import co.paulfran.taskappv2.ProjectDatabase
+import co.paulfran.taskappv2.models.Items
+import co.paulfran.taskappv2.models.ProjectWithItems
+import co.paulfran.taskappv2.models.Projects
 
 class AppData {
 
     companion object DataHolder {
-        var projects: MutableList<Project> = mutableListOf()
+        var dbFileName = "project_db"
+        lateinit var db: ProjectDatabase
+
+        var projects: MutableList<ProjectWithItems> = mutableListOf()
 
         fun initialize() {
-            val item1 = Item("Bread", false)
-            val item2 = Item("Milk", true)
+            val project1 = Projects("Home")
+            val item1 = Items("Bread", project1.name, false)
+            val item2 = Items("Milk", project1.name, true)
 
-            val item3 = Item("Tap to cross", true)
-            val item4 = Item("Long press to delete", false)
+            val projectWithItems1 = ProjectWithItems(project1, mutableListOf(item1, item2))
 
-            val project1 = Project("Home", mutableListOf(item1, item2))
-            val project2 = Project("Training", mutableListOf(item3, item4))
+            val project2 = Projects("Training")
+            val item3 = Items("Tap to cross", project2.name,true)
+            val item4 = Items("Long press to delete", project2.name, false)
 
-            projects = mutableListOf(project1, project2)
+            val projectWithItems2 = ProjectWithItems(project2, mutableListOf(item3, item4))
+
+
+            projects = mutableListOf(projectWithItems1, projectWithItems2)
         }
     }
 }
